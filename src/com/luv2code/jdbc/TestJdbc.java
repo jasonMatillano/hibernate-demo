d3976dd8e51afd1f0cf7ea1123397120d1ed7bbe
jasonMatillano <jason.matillano@gmail.com>
2018-09-10 13:31:34 +0800

    1st commit

---
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcurl = "jdbc:mysql://localhost:3310/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "root";
+		String pass = "1234";
+		
+		try {
+			System.out.println("Connecting to DB...");
+			
+			Connection myConn = DriverManager.getConnection(jdbcurl, user, pass);
+			
+			System.out.println("Connecting SUCESS!");
+			System.out.println("Connection name: " + myConn);
+			
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
